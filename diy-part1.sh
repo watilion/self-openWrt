b903d98e767535a6a6ed1543369d0655b6cab7c6
watilion <wutenglong@zeiet.com>
2026-04-02 09:08:27 +0800

    流水线环境准备脚本调整

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # Add a feed source
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
